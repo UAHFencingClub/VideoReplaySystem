cbc5602904a96de9306dc892e89e1fbaf73a9a0e
Erik Fong <quantumerikfong@gmail.com>
2023-03-08 02:29:24 -0600

    Inital Full Incorperation Test
    Moved some code from the HLS directory. It does not work as intended. The HSL stream has issues, and the CiruclarBuffer starts before it is desired.

--- PrimaryCamera/FocuserWeb.py
+++ PrimaryCamera/FocuserWeb.py
@@ -1,4 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
+import time
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput, CircularOutput, FileOutput
+from libcamera import Transform
 
 app = Flask(__name__)
 
@@ -9,9 +14,50 @@ else:
 
 camera_control = CameraController()
 
+
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration(raw={"size": (1280, 720)},transform=Transform(hflip=True,vflip=True))
+#video_config["controls"]['FrameDurationLimits']=(25000,25000)
+
+picam2.configure(video_config)
+picam2.set_controls({"FrameRate": 40})
+
+encoder = H264Encoder(10000000)
+#output = FfmpegOutput('test.mp4', audio=True)
+output1 = FfmpegOutput("-f hls -hls_time 4 -hls_list_size 20 -hls_flags delete_segments -hls_allow_cache 0 streams/stream.m3u8")
+output2 = FileOutput('full.h264')
+output3 = CircularOutput()
+output3.fileoutput = "circ.h264"
+encoder.output = [output1, output2, output3]
+
+# Start streaming to the network.
+picam2.start_encoder(encoder)
+picam2.start()
+
+
 @app.route('/')
 def index():
 	return 'Server Works!'
+
+@app.route('/replay', methods=['GET', 'POST'])
+def replay():
+	if request.method == 'POST':
+		output3.start()
+		output3.stop()
+		result = "circ.h264"
+	else:
+		result = "ToDo"
+	
+	return result
+
+@app.route('/live')
+def live():
+	return render_template('live_feed.html')
+
+@app.route('/streams/<path:path>')
+def send_streams(path):
+    return send_from_directory('streams', path)
 
 @app.route('/api/servo', methods=['GET', 'POST'])
 def server_controller():
@@ -48,3 +94,11 @@ def camera_api():
 	return result
 
 
+# print('Stopping Encoder')
+# picam2.stop_encoder()
+
+# print('Stopping')
+# #picam2.stop_recording()
+# for output in encoder.output:
+#     output.stop()
+# picam2.stop()
