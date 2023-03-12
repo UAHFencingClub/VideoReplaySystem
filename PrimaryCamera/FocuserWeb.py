019f6d700b3fb1507cb1ed6fe66e879561b6e4d2
Erik Fong <quantumerikfong@gmail.com>
2023-03-12 01:24:16 -0600

    More MJpeg feed changes

--- PrimaryCamera/FocuserWeb.py
+++ PrimaryCamera/FocuserWeb.py
@@ -63,6 +63,10 @@ def gen():
 		yield (b'--frame\r\n'
 			   b'Content-Type: image/jpeg\r\n\r\n' + byte_encode + b'\r\n')
 
+@app.route('/live_mjpeg')
+def index():
+    return render_template('mjpeg_live.html')
+
 @app.route('/video_feed')
 def video_feed():
 	return Response(gen(),
