b712e21affb64b405a4bde8aac06998719b5e9af
Erik Fong <quantumerikfong@gmail.com>
2023-03-15 22:11:42 -0500

    Initial, untested video replay setup

--- PrimaryCamera/FocuserWeb.py
+++ PrimaryCamera/FocuserWeb.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory, Response
+from flask import Flask, request, render_template, send_from_directory, Response, redirect
 import time
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
@@ -6,6 +6,7 @@ from picamera2.outputs import FfmpegOutput, CircularOutput, FileOutput
 from libcamera import Transform
 import cv2
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -30,32 +31,49 @@ encoder = H264Encoder(10000000)
 output1 = FfmpegOutput("-f hls -hls_time 4 -hls_list_size 20 -hls_flags delete_segments -hls_allow_cache 0 streams/stream.m3u8")
 output2 = FileOutput('full.h264')
 output3 = CircularOutput()
-output3.fileoutput = "circ.h264"
 encoder.output = [output1, output2, output3]
 
 # Start streaming to the network.
 picam2.start_encoder(encoder)
+#The encoder automatically calls output.start()
+output2.stop()
+output3.stop()
 picam2.start()
+
 
 
 @app.route('/')
 def index():
 	return 'Server Works!'
 
-@app.route('/replay', methods=['GET', 'POST'])
-def replay():
+REPLAY_CLIPS_DIRECTORY='/clips'
+REPLAY_CLIPS_FORMAT='mp4'
+@app.route(f'{REPLAY_CLIPS_DIRECTORY}/<path:path>')
+def get_replay_clip(path):
+	return send_from_directory('clip',path)
+
+@app.route('/replay/<id>', methods=['GET', 'POST'])
+def replay(clip_id):
 	if request.method == 'POST':
+		epoch = int(time.time())
+		replay_base_filename = f'Replay-{epoch}'
+		output3.fileoutput = f"./{REPLAY_CLIPS_DIRECTORY}/{replay_base_filename}.h264"
 		output3.start()
 		output3.stop()
-		result = "circ.h264"
-	else:
-		result = "ToDo"
+		#TODO implement audio buffer to dump
+		#TODO Start background ffmpeg task to convert to mp4
+		result = redirect(f"/replay/{replay_base_filename}.{REPLAY_CLIPS_FORMAT}", code=301)
+	elif clip_id is None:
+		clips_list = [clip for clip in os.listdir(f"./{REPLAY_CLIPS_DIRECTORY}") if clip.endswith(f".{REPLAY_CLIPS_FORMAT}")]
+		result = render_template('replay_list.html',clips_list=clips_list, clip_path = REPLAY_CLIPS_DIRECTORY)
+	else: 
+		result = render_template('replay_interface.html',clip_id = clip_id, clip_path = REPLAY_CLIPS_DIRECTORY)
 	
 	return result
 
 def gen():
 	while True:
-		yuv_frame = picam2.capture_array("lores")
+		yuv_frame = picam2.capture_array("main")
 		rgb_frame = cv2.cvtColor(yuv_frame, cv2.COLOR_YUV420p2RGB)
 		_, jpeg_encoded = cv2.imencode('.jpg', rgb_frame)
 		data_encode = np.array(jpeg_encoded)
