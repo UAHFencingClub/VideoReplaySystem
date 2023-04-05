059a8b900c65c2d0f4d02ccf0285e2a6547b807b
Erik Fong <quantumerikfong@gmail.com>
2023-04-05 16:44:35 -0500

    Working camera control interface.
    Better boundaries needed.

--- MotionCamera/trackingBox.py
+++ MotionCamera/trackingBox.py
@@ -1,19 +1,24 @@
 import numpy
-from websockets.sync.client import connect
+import socketio
 import json
+import rel
 
 class CameraMove:
-    def __init__(self, camera_ip) -> None:
-        self.ip = camera_ip
-        self.websocket = connect("ws://"+camera_ip)
-
-    def move(self, angle):
-        assert(abs(angle)<=90)
-        event = {
-            "type": "my event",
-            "motor_x": angle/90,
+    def __init__(self, url) -> None:
+        self.url = url
+        self.sio = socketio.Client()
+        self.sio.connect(url)
+        
+    def move(self, yaw, pitch):
+        #should be handled on the other end, will do later.
+        assert(abs(yaw)<=90)
+        assert(pitch<0)
+        assert(pitch>=-90)
+        data = {
+            "motor_x": yaw/90,
+            "motor_y": pitch/90,
         }
-        self.websocket.send(json.dumps(event))
+        self.sio.emit('my event', data)
 
 def center(boundingBox):
     centerX = int(boundingBox[0]) + int(boundingBox[2]/2)
