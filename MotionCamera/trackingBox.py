d654777f421e03ec3972f79c5e5d93d2ebffbea7
Abram Coleman <ajc0043@uah.edu>
2023-04-08 08:17:09 -0500

    tracking algorithm

--- MotionCamera/trackingBox.py
+++ MotionCamera/trackingBox.py
@@ -5,7 +5,24 @@ def center(boundingBox):
     centerY = int(boundingBox[1]) + int(boundingBox[3]/2)
     return centerX, centerY
 
-def movement(frameWidth,frameHeight,rightFencerX,rightFencerY,leftFencerX,leftFencerY):
-    
-    
-    return moveLeft, moveRight, zoomout, zoomin
+def movementOfMovingCamera(frameWidth,Fencer1X,Fencer2X):
+    moveLeft = 0
+    moveRight = 0
+    leftBoundry = 50
+    rightBoundry = frameWidth - 50
+
+    #fencers are centered in the camera
+    if (((Fencer1X < leftBoundry) and (Fencer1X > rightBoundry)) and ((Fencer2X < leftBoundry) and (Fencer2X > rightBoundry))):
+        moveLeft = 0
+        moveRight = 0
+    #F
+    elif (((Fencer1X > leftBoundry) and (Fencer1X < rightBoundry)) and ((Fencer2X > leftBoundry) and (Fencer2X < rightBoundry))):
+        moveLeft = 1
+        moveRight = 0
+    elif (((Fencer1X > leftBoundry) and (Fencer1X < rightBoundry)) and ((Fencer2X > leftBoundry) and (Fencer2X < rightBoundry))):
+        moveLeft = 0
+        moveRight = 1
+    elif (((Fencer1X > leftBoundry) and (Fencer1X < rightBoundry)) and ((Fencer2X > leftBoundry) and (Fencer2X < rightBoundry))):
+        moveRight = 0
+        moveLeft = 0
+    return moveLeft, moveRight
