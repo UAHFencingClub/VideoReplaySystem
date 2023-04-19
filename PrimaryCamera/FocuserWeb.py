e3c36c00c89470f5993142dd2092dfa61d1ab52d
Erik Fong <quantumerikfong@gmail.com>
2023-04-19 06:02:29 +0100

    Added liscense to top

--- PrimaryCamera/FocuserWeb.py
+++ PrimaryCamera/FocuserWeb.py
@@ -1,3 +1,22 @@
+'''
+    Fencing Video Tracker
+    Copyright (C) 2023  Erik Fong, Abram Coleman, Nathan Wight, Alex Rankin for the University of Alabama in Huntsville
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
 from flask import Flask, request, render_template, send_from_directory, Response, redirect
 from flask_socketio import SocketIO, emit
 
